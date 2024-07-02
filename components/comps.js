0f08ffd02a82e2575f6a388e54c14ce67a897564
Jeff Kershaw <jeff.kershaw@blippar.com>
2024-07-02 11:41:05 +0100

    update to projects for lighting shadows and some components

---
+++ components/comps.js
@@ -0,0 +1,52 @@
+AFRAME.registerComponent('floorshadow', {
+  init: function() {
+    let el = this.el;
+    let self = this;
+    let mesh = el.getObject3D('mesh');
+    if (!mesh) { return; }
+    mesh.material = new THREE.ShadowMaterial();
+    mesh.material.opacity = 0.5;
+  }
+});
+AFRAME.registerComponent('nocull', {
+  init() {
+      this.el.addEventListener("model-loaded", this.update.bind(this));
+  },
+  update() {
+      const mesh = this.el.getObject3D("mesh");
+      if (!mesh) {
+          return;
+      }
+      mesh.traverse(function(node) {
+          if (node.isMesh) {
+              node.frustumCulled = false;
+          }
+      });
+  },
+});
+AFRAME.registerComponent("eq-env-map", {
+  schema: {
+      map: { default: './images/envmap.jpg' }
+  },  
+  init: function () {
+      this.el.addEventListener('model-loaded', this.update.bind(this));
+  },
+  update: function () {
+      const mesh = this.el.getObject3D('mesh');
+      const currentData = this.data;
+      var texture = new THREE.TextureLoader().load(currentData.map, function () {
+          const sceneEl = document.querySelector("a-scene");
+          var targetCube = new THREE.WebGLCubeRenderTarget(512);
+          var renderer = sceneEl.renderer;
+          var cubeTex = targetCube.fromEquirectangularTexture(renderer, texture);
+          if (mesh === undefined) return;
+          mesh.traverse(function (node) {
+              if (node.material) {
+                  node.material.envMap = cubeTex.texture;
+                  node.material.envMap.intensity = 1;
+                  node.material.needsUpdate = true;
+              }
+          });
+      });
+  }
+});
